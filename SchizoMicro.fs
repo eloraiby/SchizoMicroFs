b39e56a439f3907aef33abadb98b510700843350
eloraiby <wael.eloraiby@gmail.com>
2015-08-15 21:57:21 -0400

    parser 1st pass

--- SchizoMicro.fs
+++ SchizoMicro.fs
@@ -1,21 +1,44 @@
 ﻿open System
 
-type Token =
-    | TChar     of char
-    | TInt64    of int64
-    | TReal64   of double
-    | TString   of string
-    | TSymbol   of string
-    | LParen
-    | RParen
-    | LBrace
-    | RBrace
-    | LBrack
-    | RBrack
-    | Tag
-    | Comma
-    | SC
+type CellData =
+    | CUnit
+    | CBoolean  of bool
+    | CChar     of char
+    | CInt64    of int64
+    | CReal64   of double
+    | CString   of string
+    | CSymbol   of string
+    | CList     of Cell list
+    | NativeLambda  of NativeCall
+    | NativeMacro   of NativeCall
+    | Lambda    of NormalCall
+    | Macro     of NormalCall
+    | Environment of Map<string, Cell>
 
+and NativeCall = {
+    ArgCount    : int
+    FFI         : Cell[] -> Cell
+}
+
+and NormalCall = {
+    Args        : string[]
+    Body        : Cell list
+}
+
+and Cell = {
+    Data        : CellData
+    Tags        : Cell list
+    DebugInfo   : DebugInfo option
+}
+
+and DebugInfo = {
+    Line        : int
+    Offset      : int
+}
+
+type CellData
+with
+    member x.ToCell = { Cell.Data = x; Tags = []; DebugInfo = None }
 let isAlpha ch =
     if (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
     then true
@@ -35,27 +58,21 @@ let isSpecial =
     | '>'   | '|'   | '~'   | '.' -> true
     | _   -> false
 
-let isWS = function
-    | ' ' 
-    | '\n'
-    | '\r' -> true
-    | _    -> false
-
 let readChar (str: char list) =
     match str with
     | '\\' :: ch :: '\'' :: t ->
         match ch with
-        | 'n'  -> TChar '\n', t
-        | 'r'  -> TChar '\r', t
-        | 'a'  -> TChar '\a', t
-        | '\'' -> TChar '\'', t
+        | 'n'  -> (CChar '\n').ToCell, t
+        | 'r'  -> (CChar '\r').ToCell, t
+        | 'a'  -> (CChar '\a').ToCell, t
+        | '\'' -> (CChar '\'').ToCell, t
         | _    -> failwith (sprintf "unknown escape character %c after \\ in char" ch)
-    | ch   :: '\'' :: t       -> TChar ch, t
+    | ch   :: '\'' :: t       -> (CChar ch).ToCell, t
     | _               -> failwith "not a well formed char"
 
 let rec readString (acc: char list) (str: char list) =
     match str with
-    | '"'  :: t -> TString (System.String(acc |> List.rev |> Array.ofList)), t
+    | '"'  :: t -> (CString (System.String(acc |> List.rev |> Array.ofList))).ToCell, t
     | '\\' :: ch :: t ->
         let newCh =
             match ch with
@@ -71,8 +88,7 @@ let rec readString (acc: char list) (str: char list) =
 let rec readSymbol (acc: char list) (str: char list) =
     match str with
     | ch :: t when isAlpha ch || isDigit ch || isSpecial ch -> readSymbol (ch :: acc) t
-    | ch :: t when isWS ch  -> TSymbol (System.String(acc |> List.rev |> Array.ofList)), t      // white space, bail
-    | _                     -> TSymbol (System.String(acc |> List.rev |> Array.ofList)), str    // anything else, just bail
+    | _                     -> (CSymbol (System.String(acc |> List.rev |> Array.ofList))).ToCell, str    // anything else, just bail
 
 let readNumber (str: char list) =
     let rec readInt (acc: char list) (str: char list) =
@@ -97,54 +113,56 @@ let readNumber (str: char list) =
     let intstr = System.String (integral |> List.toArray)
 
     if intstr.Length <> 0
-    then TInt64 (Convert.ToInt64 intstr), nextList
+    then (CInt64 (Convert.ToInt64 intstr)).ToCell, nextList
     else failwith "invalid number"
     
-let rec lex (tlist: Token list) (str: char list) : Token list =
+let rec skipWS (str: char list) =
+    let isWS = function
+        | ' ' 
+        | '\n'
+        | '\r' -> true
+        | _    -> false
+
     match str with
-    | '\'' :: t -> let ch,  nextList = readChar t      in lex (ch  :: tlist) nextList
-    | '"'  :: t -> let str, nextList = readString [] t in lex (str :: tlist) nextList
-    | '('  :: t -> lex (LParen :: tlist) t
-    | ')'  :: t -> lex (RParen :: tlist) t
-    | '{'  :: t -> lex (LBrace :: tlist) t
-    | '}'  :: t -> lex (RBrace :: tlist) t
-    | '['  :: t -> lex (LBrack :: tlist) t
-    | ']'  :: t -> lex (RBrack :: tlist) t
-    | ','  :: t -> lex (Comma  :: tlist) t
-    | ';'  :: t -> lex (SC     :: tlist) t
-    | '+'  :: ch :: t
-    | '-'  :: ch :: t
-    | ch   :: t when isDigit ch -> let num, nextList = readNumber str in lex (num :: tlist) nextList
-    | ch   :: t when isAlpha ch  || isSpecial ch -> let sym, nextList = readSymbol [] str in lex (sym :: tlist) nextList
-    | ch   :: t when isWS ch -> lex tlist t
-    | [] -> List.rev tlist
-    | _ -> failwith "ill formed S-Expression"
+    | ch :: t when isWS ch -> skipWS t
+    | _ -> str
 
-type Cell =
-    | CUnit
-    | CChar     of char
-    | CInt64    of int64
-    | CReal64   of double
-    | CString   of string
-    | CSymbol   of string
-    | CList     of Cell list
+let rec readList (acc: Cell list) (str: char list) =
+    let str = skipWS str
+    match str with
+    | []        -> failwith "list doesn't have an end ')'"
+    | ')' :: t  ->
+        match acc with
+        | [] -> CUnit.ToCell, t
+        | _  -> (CList (acc |> List.rev)).ToCell, t
+    | _         -> let tok, nextList = nextToken str in readList (tok :: acc) nextList
 
-with
-    static member fromToken (t: Token) =
-        match t with
-        | TChar     t -> CChar   t
-        | TInt64    t -> CInt64  t
-        | TReal64   t -> CReal64 t
-        | TString   t -> CString t
-        | TSymbol   t -> CSymbol t
-        | _           -> failwith "unrecognized token"
+and nextToken (str: char list) : Cell * char list =
+    let isCut =
+        function
+        | ')' | ' ' | '\r' | '\n' | '\t' -> true
+        | _ -> false
 
-let rec readSExp (acc: Cell list) (tlist: Token list) : Cell * (Token list) =
-    match tlist with
-    | LParen :: RParen :: t -> readSExp (CUnit :: acc) t
-    | LParen :: t -> readSExp [] t
-    | RParen :: t -> CList (List.rev acc), t
-    | cell   :: t -> readSExp ((Cell.fromToken cell) :: acc) t
+    let str = skipWS str
+    match str with
+    | '\'' :: t -> readChar t
+    | '"'  :: t -> readString [] t
+    | '('  :: t -> readList [] t
+    | 't' :: 'r' :: 'u' :: 'e' :: t -> (CBoolean true).ToCell, t
+    | 'f' :: 'a' :: 'l' :: 's' :: 'e' :: t -> (CBoolean false).ToCell, t
+    | sign :: ch :: t when (sign = '+' || sign = '-') && isDigit ch -> readNumber str
+    | ch   :: t when isDigit ch -> readNumber str 
+    | ch   :: t when isAlpha ch  || isSpecial ch -> readSymbol [] str
+    | _ -> failwith "ill formed S-Expression" 
+
+let parse (str: char list) : Cell list =
+    let rec loop (acc: Cell list) (str: char list) : Cell list =
+        match str with
+        | [] -> List.rev acc
+        | _ ->
+            let tok, nextList = nextToken str
+            loop (tok :: acc) nextList
+    loop [] str
 
 [<EntryPoint>]
 let main argv =
@@ -155,16 +173,18 @@ let main argv =
         "0"
         "symbol"
         "\"string\""
-        "("
-        ")"
+        "( )"
         "'\\n'"
         "a b c d"
         "(abc def gfhi)"
         "(abc123 d045ef gf.hi)"
         "123 () 456 a b 123.hh !hello $bla%bla()aha"
+        "(a b (10 12 cd) (10 11 12))"
+        "false"
+        "true"
     |]
     |> Array.iter
         (fun e ->
-            printfn "%A" (lex [] (e |> Seq.toList)))
+            printfn "%A" (parse (e |> Seq.toList)))
 
     0 // return an integer exit code
